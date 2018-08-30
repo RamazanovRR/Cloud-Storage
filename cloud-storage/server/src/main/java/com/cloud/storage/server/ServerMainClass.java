de66b81305f92dbc7cf877fb08f168359aa2fe47
RamazanovRR <Citykimmi@yandex.ru>
2018-08-30 19:38:42 +0700

    Дополнительные файлы

--- cloud-storage/server/src/main/java/com/cloud/storage/server/ServerMainClass.java
+++ cloud-storage/server/src/main/java/com/cloud/storage/server/ServerMainClass.java
@@ -3,5 +3,17 @@ package com.cloud.storage.server;
 public class ServerMainClass {
     public static void main(String[] args) {
 
+        Database db = new Database();
+        db.connect();
+        db.createTable();
+        db.addUserInTable(addArr(1));
+        db.addUserInTable(addArr(2));
+        db.addUserInTable(addArr(3));
+        db.disconnect();
+    }
+
+    public static String[] addArr(int num) {
+        String[] testArr = {"test" + num, "test" +num, "test" +num, "test" +num, "test" +num, "test" +num, "test" +num};
+        return testArr;
     }
 }
