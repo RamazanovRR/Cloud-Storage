5c8f59d1767842a0c6d47bb5e1f5bea67f272171
RamazanovRR <Citykimmi@yandex.ru>
2018-09-06 12:19:21 +0700

    доработана логика для кнопки "вход" происходит авторизация и переход на главное окно программы

--- cloud-storage/client/src/main/java/com/cloud/storage/client/LoginController.java
+++ cloud-storage/client/src/main/java/com/cloud/storage/client/LoginController.java
@@ -1,6 +1,8 @@
 package com.cloud.storage.client;
 
 import animationObject.Shiver;
+import com.cloud.storage.common.AuthPackage;
+import com.cloud.storage.common.Encryption;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -29,8 +31,31 @@ public class LoginController {
     public void loginIn() {
         String fieldLogin = textFieldLogin.getText().trim();
         String fieldPass = textFieldPassword.getText().trim();
+        AuthPackage auth;
 
         if(!fieldLogin.equals("") && !fieldPass.equals("")) {
+            textFieldLogin.setText("");
+            textFieldPassword.setText("");
+
+            auth = new AuthPackage(fieldLogin, Encryption.encode(fieldPass, "крафт"));
+            ClientConnection.getInstance().connection();
+            Object obj = ClientConnection.getInstance().sendPackage(auth);
+            if(obj == null) System.out.println("Объект auth от сервера равен null");
+            auth = (AuthPackage) obj;
+            if(auth.isAuth()) {
+                try {
+                    ManagerWindow.getInstance().openCloudStorageWindow(login, fieldLogin);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // реализовать в методе
+                Shiver fieldLog = new Shiver(textFieldLogin);
+                Shiver fieldPas = new Shiver(textFieldPassword);
+                fieldLog.startAnimation();
+                fieldPas.startAnimation();
+            }
+
             // метод авторизации пользователя.
         } else {
             Shiver fieldLog = new Shiver(textFieldLogin);
